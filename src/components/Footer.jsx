9f40efd1ac7f2b5648e514803ae450b77c500710
Ravi Pandey <imravipanday@gmail.com>
2024-05-25 19:37:53 +0530

    Forget to add links 😛

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,19 +7,19 @@ const Footer = () => {
   return (
     <div className="fixed inset-x-0 bottom-0 sm:h-22 h-auto w-full flex flex-col sm:flex-row items-center justify-between bg-purple-400 bg-opacity-30 backdrop-blur-sm p-4 px-10">
       <h2 className="text-purple-700 text-sm mb-2 sm:mb-0">Developed in India 🇮🇳</h2>
-      <div className="flex space-x-16 md:space-x-8">
-        <button className="flex flex-col items-center justify-center text-purple-700 hover:text-purple-800">
+      <div className="flex space-x-6">
+        <a href="https://github.com/mrravipandee/the-dictionary/" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center justify-center text-purple-700 hover:text-purple-900">
           <FaGithub size={20} />
           <span className="text-xs">GitHub</span>
-        </button>
-        <button className="flex flex-col items-center justify-center text-purple-700 hover:text-purple-800">
+        </a>
+        <a href="mailto:imravipanday@gmail.com" className="flex flex-col items-center justify-center text-purple-700 hover:text-purple-900">
           <IoIosContact size={20} />
           <span className="text-xs">Contact</span>
-        </button>
-        <button className="flex flex-col items-center justify-center text-purple-700 hover:text-purple-800">
+        </a>
+        <a href="https://dictionaryapi.dev/" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center justify-center text-purple-700 hover:text-purple-900">
           <TbApi size={20} />
           <span className="text-xs">API</span>
-        </button>
+        </a>
       </div>
     </div>
   );
